8a8c3f025399ca0a045c00928a5d36778cc5f86e
alanwu04 <alanwu0004@gmail.com>
2023-11-28 17:03:04 -0800

    Finished base implementation for keyboard test

--- keyboard-test.js
+++ keyboard-test.js
@@ -1,7 +1,31 @@
 // Constants
-const keyboardInput = document.getElementById("keyboard-input");
+const keyboardDisplay = document.getElementById("keyboard-display");
+const restart = document.getElementById("restart");
+const keyMap = {};
 
-// Actions when loading the webpage
-window.onload = () => {
-    keyboardInput.focus();
+// Add every key to a map
+document.querySelectorAll(".key").forEach(key => {
+    keyMap[key.id] = key;
+})
+
+// Keyboard inputs for the webpage
+document.addEventListener("keydown", function (e) {
+    // Reset the typing test by pressing ESC
+    // e.preventDefault();
+
+    if (keyMap[e.code]) {
+        keyMap[e.code].classList.add("pressed");
+    }
+})
+
+// Reset the keyboard test
+const resetKeyboardTest = () => {
+    document.querySelectorAll(".key").forEach(key => {
+        key.classList.remove("pressed");
+    })
+}
+
+// Reset the keybaord test when clicking the shortcut hint
+restart.onclick = function () {
+    resetKeyboardTest();
 }
